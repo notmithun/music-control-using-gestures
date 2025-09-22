42472358dd36a6c3d12fbdb1edd13112e44aebc2
Mithun <110370009+notmithun@users.noreply.github.com>
2025-09-22 21:17:52 +0530

    final thing done

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ mixer.init()
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+print("hands")
 
 MUSIC_FOLDER = "songs"
 playlist = [f for f in os.listdir(MUSIC_FOLDER) if f.endswith((".mp3", ".wav", ".ogg"))]
 playlist.sort()
 current_index = 0
 playing = False
+print(MUSIC_FOLDER)
 
-def play_song(index):
+def play_song(index: int) -> str:
+    """Plays the song
+
+    Args:
+        index (int): Index of the songs in the playlist
+
+    Returns:
+        str: the song path
+    """
     global playing
     song_path = os.path.join(MUSIC_FOLDER, playlist[index])
     mixer.music.load(song_path)
@@ -51,21 +61,18 @@ def detect_gesture(results, frame):
     
     if results.multi_hand_landmarks:
         for lm in results.multi_hand_landmarks:
-            # draw landmarks
             mp_drawing.draw_landmarks(frame, lm, mp_hands.HAND_CONNECTIONS)
-
-            # fingertip landmarks
-            tip_ids = [8, 12, 16, 20]  # index, middle, ring, pinky
-            fingers = [lm[tip].y < lm[tip - 2].y for tip in tip_ids]  # True if extended
-
-            # detect gestures
+            tip_ids = [8, 12, 16, 20]
+            fingers = [lm.landmark[tip].y < lm.landmark[tip - 2].y for tip in tip_ids]
             if all(not f for f in fingers):
                 return "fist"
             elif all(f for f in fingers):
+                print("if 2")
                 return "open"
             else:
-                dx = abs(lm[4].x - lm[8].x)
-                dy = abs(lm[4].y - lm[8].y)
+
+                dx = abs(lm.landmark[4].x - lm.landmark[8].x)
+                dy = abs(lm.landmark[4].y - lm.landmark[8].y)
                 if dx < 0.05 and dy < 0.05:
                     return "pinch"
 
@@ -81,10 +88,10 @@ while True:
     if not ret:
         raise CameraError("Failed to access the camera!")
     frame = cv2.flip(frame, 1)
-    rgb_frame = cv2.cvtColor(frame, cv2.BGR2RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(rgb_frame)
 
-    gesture = detect_gesture()
+    gesture = detect_gesture(results=results, frame=frame)
 
     if gesture == "fist" and playing:
         stop_song()
@@ -97,6 +104,7 @@ while True:
     
     text = playlist[current_index] if playing else "Stopped"
     cv2.putText(frame, text, (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 140, 255), 2)
+    print(4)
 
     cv2.imshow("Music Control using Gestures", frame)
 
